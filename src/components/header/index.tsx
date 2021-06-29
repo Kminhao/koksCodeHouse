9f29da4bfb6aa914c219bdd7d90ee5f4ccb7c199
Gabriel <gabri.elf@hotmail.com>
2021-06-29 19:26:45 -0300

    home page tsparticles added

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,17 +1,22 @@
-import react from 'react';
+import React from 'react';
 import './style.css';
+import Particles from "react-tsparticles";
+
 
 
 
 function header() {
-    return(
+    return (
         <div className="headerContainer">
-            <ul className="headerLinks">
-                <li><a href="">Home</a></li>
-                <li><a href="">Galeria</a></li>
-                <li><a href="">Enviar a braba</a></li>
-                <li><a href="">Rapeize</a></li>
-            </ul>
+           
+                <ul className="headerLinks">
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Galeria</a></li>
+                    <li><a href="">Enviar a braba</a></li>
+                    <li><a href="">Rapeize</a></li>
+                </ul>
+
+
         </div>
     );
 }
--- src/pages/landingPage/index.tsx
+++ src/pages/landingPage/index.tsx
@@ -1,12 +1,32 @@
 import react from 'react';
 import './style.css';
+import Particles from "react-tsparticles";
 import Header from '../../components/header';
+
 
 
 function landingPage() {
     return(
         <div className="landingContainer">
             <Header />
+
+            <div className="canvas">
+                <Particles
+                    options={{
+                        "particles": {
+                            "links": {
+                                "enable": true
+                            },
+                            "move": {
+                                "enable": true
+                            },
+                            "size": {
+                                "value": 2
+                            }
+                        }
+                    }}
+                />
+            </div>
         </div>
     );
 }
